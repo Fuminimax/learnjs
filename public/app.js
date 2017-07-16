98aa1cf31240af0b52cf31d5a5d1f75c62bbb581
Fumio Takayama <fumio.takayama@gmail.com>
2017-07-16 21:09:26 +0900

    modified:   app.js 	modified:   index.html 	modified:   tests/app_spec.js

--- public/app.js
+++ public/app.js
@@ -1,7 +1,23 @@
 'use strict';
 var learnjs = {};
 
+learnjs.problemView = function() {
+    return $('<div class="problem-view">').text('Coming soon!');
+}
+
+learnjs.showView = function(hash) {
+    var routes = {
+        '#problem-1': learnjs.problemView
+    };
+    var viewFn = routes[hash];
+    if (viewFn) {
+        $('.view-container').empty().append(viewFn());
+    }
+}
+
+/*
 learnjs.showView = function(hash) {
     var problemView = $('<div class="problem-view">').text('Coming soon!');
-    $('.view-container').empty().append(problemView); 
+    $('.view-container').empty().append(problemView);
 }
+*/
--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -3,4 +3,8 @@ describe('LearnJSの2.1.2のテスト', function() {
         learnjs.showView('#problem-1');
         expect($('.view-container .problem-view').length).toEqual(1);
      });
+     it('showView()へページの識別子(hash)を渡さない時', function() {
+        learnjs.showView('');
+        expect($('.view-container .landing-view').length).toEqual(1);
+    });
 });
